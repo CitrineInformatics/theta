ee7708601692790a77caba61430575a413fd7ad9
Max Hutchinson <maxhutch@gmail.com>
2017-03-29 17:17:05 -0500

    Pulling the run method into the Benchmark trait

--- src/main/scala/io/citrine/theta/benchmarks/RandomGenerationBenchmark.scala
+++ src/main/scala/io/citrine/theta/benchmarks/RandomGenerationBenchmark.scala
@@ -1,24 +1,18 @@
 package io.citrine.theta.benchmarks
 
-import io.citrine.theta.Stopwatch
-
 import scala.util.Random
 
 /**
+  * Benchmark that generates random numbers
   * Created by maxhutch on 3/29/17.
   */
 object RandomGenerationBenchmark extends Benchmark {
-  /**
-    * Run the benchmark
-    *
-    * @return the benchmark time (in seconds)
-    */
-  override def run(): Double = {
-    Stopwatch.time(kernel, benchmark = "None", nWarm = 8, nTrial = 8)
-  }
 
-  def kernel(): Unit = {
-    (0 until 4 * 1048576).foreach{i =>
+  /**
+    * Generate 2^20 random numbers using Scala.Random.nextDouble()
+    */
+  override def kernel(): Unit = {
+    (0 until 1048576).foreach{i =>
       Random.nextDouble()
     }
   }
