479fdc84f0587aaf1fc56388565414c6e241cc38
Max Hutchinson <maxhutch@gmail.com>
2017-03-29 17:17:05 -0500

    Pulling the run method into the Benchmark trait

--- src/main/scala/io/citrine/theta/benchmarks/Benchmark.scala
+++ src/main/scala/io/citrine/theta/benchmarks/Benchmark.scala
@@ -1,13 +1,24 @@
 package io.citrine.theta.benchmarks
 
+import io.citrine.theta.Stopwatch
+
 /**
+  * Benchmarks that can be used to non-dimensionalize time
   * Created by maxhutch on 3/29/17.
   */
 trait Benchmark {
 
   /**
-    * Run the benchmark
+    * Run the benchmark a couple times to get a solid reading
+    *
     * @return the benchmark time (in seconds)
     */
-  def run(): Double
+  def run(): Double = {
+    Stopwatch.time(kernel(), benchmark = "None", nWarm = 8, nTrial = 8)
+  }
+
+  /**
+    * Workload that will be timed for the benchmark
+    */
+  def kernel(): Unit
 }
