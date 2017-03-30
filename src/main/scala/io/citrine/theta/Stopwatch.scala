16a12ab4fbda05e9480c6e4e3de48398ca0dded6
Max Hutchinson <maxhutch@gmail.com>
2017-03-30 08:16:25 -0500

    That didn't work; what about nWarn

--- src/main/scala/io/citrine/theta/Stopwatch.scala
+++ src/main/scala/io/citrine/theta/Stopwatch.scala
@@ -7,7 +7,7 @@ object Stopwatch {
     * @param block block to run
     * @return the non-dimensional time of the block
     */
-  def time[R](block: => R, benchmark: String = "Default", nWarm: Int = 8, nTrial: Int = 4): Double = {
+  def time[R](block: => R, benchmark: String = "Default", nWarm: Int = 4, nTrial: Int = 8): Double = {
     (0 until nWarm).foreach(i => block)
     val start = System.nanoTime()
     (0 until nTrial).foreach(i => block)
