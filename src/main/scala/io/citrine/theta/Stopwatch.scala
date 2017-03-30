80b62fe85b86ccd806a4bca3061985338c57b84f
Max Hutchinson <maxhutch@gmail.com>
2017-03-30 08:12:05 -0500

    Rolling back nTrial

--- src/main/scala/io/citrine/theta/Stopwatch.scala
+++ src/main/scala/io/citrine/theta/Stopwatch.scala
@@ -7,7 +7,7 @@ object Stopwatch {
     * @param block block to run
     * @return the non-dimensional time of the block
     */
-  def time[R](block: => R, benchmark: String = "Default", nWarm: Int = 8, nTrial: Int = 8): Double = {
+  def time[R](block: => R, benchmark: String = "Default", nWarm: Int = 8, nTrial: Int = 4): Double = {
     (0 until nWarm).foreach(i => block)
     val start = System.nanoTime()
     (0 until nTrial).foreach(i => block)
