9ac5568a483a519bea83b2f1c186bc063c74e171
Max Hutchinson <maxhutch@gmail.com>
2017-06-26 15:08:28 -0500

    Changes to defaults intended to improve stability

--- src/test/scala/io/citrine/theta/BenchmarkRegistryTest.scala
+++ src/test/scala/io/citrine/theta/BenchmarkRegistryTest.scala
@@ -1,8 +1,10 @@
 package io.citrine.theta
 
-import io.citrine.theta.benchmarks.RandomGenerationBenchmark
+import io.citrine.theta.benchmarks.{RandomGenerationBenchmark, StreamBenchmark}
 import org.junit.Test
 import org.junit.experimental.categories.Category
+
+import scala.util.Random
 
 /**
   * Created by maxhutch on 3/29/17.
@@ -21,15 +23,36 @@ class BenchmarkRegistryTest {
 
   @Test
   @Category(Array(classOf[SlowTest]))
-  def testConsistency(): Unit = {
+  def testConsistencyRandomGeneration(): Unit = {
     (0 until 32).foreach{ i =>
       val theta: Double = Stopwatch.time(RandomGenerationBenchmark.kernel(), benchmark = "RandomGeneration")
-      assert(theta < 1.4, s"RandomGeneration benchmark inconsistent (too slow ${theta})")
-      assert(theta > 0.6, s"RandomGeneration benchmark inconsistent (too fast ${theta})")
+      assert(theta < 1.1, s"RandomGeneration benchmark inconsistent (too slow ${theta})")
+      assert(theta > 0.9, s"RandomGeneration benchmark inconsistent (too fast ${theta})")
     }
-
   }
 
+  @Test
+  @Category(Array(classOf[SlowTest]))
+  def testConsistencyStream(): Unit = {
+    val N = StreamBenchmark.N
+    val a = new Array[Double](N)
+    val b = new Array[Double](N)
+    val c = new Array[Double](N)
+    (0 until N).foreach{i =>
+      a(i) = 2.0
+      b(i) = 0.5
+      c(i) = 0.0
+    }
+    (0 until 8).foreach{ i =>
+      val theta: Double = Stopwatch.time(StreamBenchmark.customKernel(a, b, c, Random.nextDouble()), benchmark = "STREAM")
+      assert(theta < 1.1, s"RandomGeneration benchmark inconsistent (too slow ${theta})")
+      assert(theta > 0.9, s"RandomGeneration benchmark inconsistent (too fast ${theta})")
+    }
+  }
+}
 
-
+object BenchmarkRegistryTest {
+  def main(argv: Array[String]): Unit = {
+    new BenchmarkRegistryTest().testConsistencyStream()
+  }
 }
