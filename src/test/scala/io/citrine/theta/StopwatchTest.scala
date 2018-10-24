e10a9fe0dffde495fcfe9baf2392be4e5ffbd128
Ryan Latture <ryan.latture@gmail.com>
2018-10-24 12:49:30 -0700

    Added tests.

--- src/test/scala/io/citrine/theta/StopwatchTest.scala
+++ src/test/scala/io/citrine/theta/StopwatchTest.scala
@@ -1,6 +1,6 @@
 package io.citrine.theta
 
-import io.citrine.theta.benchmarks.{RandomGenerationBenchmark, StreamBenchmark}
+import io.citrine.theta.benchmarks.{Benchmark, RandomGenerationBenchmark, StreamBenchmark}
 import org.junit.Test
 
 import scala.util.Random
@@ -34,6 +34,72 @@ class StopwatchTest {
     assert(theta > 0.8, s"STREAM benchmark inconsistent (too fast ${theta})")
   }
 
+  /**
+    * Test that the stopwatch can detect whether a benchmark with a known execution time is slower (or faster) than a specified time.
+    */
+  @Test
+  def testIsSlowerThan(): Unit = {
+    val b = new RandomSleepBenchmark()
+    val effectSizes = Seq(0.5, 0.8) // test medium and large effect sizes
+    effectSizes.foreach { effectSize =>
+      val delta = b.std * effectSize
+      val fasterTime = (b.mean - delta) / 1000
+      val slowerTime = (b.mean + delta) / 1000
+      assert(
+        Stopwatch.isSlowerThan(b.kernel(), fasterTime), s"Unable to determine a benchmark is slower than a known faster time with effect size $effectSize."
+      )
+      assert(
+        !Stopwatch.isSlowerThan(b.kernel(), slowerTime), s"Unable to determine a benchmark is faster than a known slower time with effect size $effectSize."
+      )
+    }
+  }
+
+  /**
+    * Test that the stopwatch whether the stopwatch will exit early if an acceptable effect size is measured
+    */
+  @Test
+  def testIsSlowerThanEffectSizeEarlyExit(): Unit = {
+    val b = new RandomSleepBenchmark()
+
+    // the measured effect size should by +/- 2 (much larger than +/- 0.5 required to terminate the test)
+    val muchFasterTime = (b.mean - b.std * 2) / 1000
+    val muchSlowerTime = (b.mean + b.std * 2) / 1000
+
+    // set FP/FN rates so low that the required number of samples won't be satisfied before an acceptable effect size is measured
+    val stringentProbability = 1.0e-6
+
+    val minRun = 4
+    val maxExpectedDuration = b.mean * (minRun + 1)
+
+    val start1 = System.nanoTime()
+    val isSlowerThanFasterTime = Stopwatch.isSlowerThan(b.kernel(), muchFasterTime, minRun = minRun, acceptableEffectSize = Some(0.5), falseNegative = stringentProbability, falsePositive = stringentProbability)
+    val duration1 = (System.nanoTime() - start1) * 1.0e-9
+    assert(duration1 < maxExpectedDuration, s"Benchmark did not exit early when effect size is much larger than acceptable limit.")
+    assert(isSlowerThanFasterTime, "Unable to determine a benchmark is slower than a known faster time after measuring acceptable effect size.")
+
+    val start2 = System.nanoTime()
+    val isSlowerThanSlowerTime = Stopwatch.isSlowerThan(b.kernel(), muchSlowerTime, minRun = minRun, acceptableEffectSize = Some(-0.5), falseNegative = stringentProbability, falsePositive = stringentProbability)
+    val duration2 = (System.nanoTime() - start2) * 1.0e-9
+    assert(duration2 < maxExpectedDuration, s"Benchmark did not exit early when effect size is much larger than acceptable limit.")
+    assert(!isSlowerThanSlowerTime, "Unable to determine a benchmark is faster than a known slower time after measuring acceptable effect size.")
+  }
+}
+
+/**
+  * Test benchmark that sleeps for a random amount of time sampled from a normal distribution.
+  */
+class RandomSleepBenchmark() extends Benchmark {
+  /** Average sleep time (in ms) */
+  val mean = 100.0 // ms
+
+  /** Standard deviation about the mean sleep time (in ms) */
+  val std = 10.0 // ms
+
+  /** Kernel that sleep for a random amount of time */
+  override def kernel(): Unit = {
+    val r = mean + Random.nextGaussian() * std
+    Thread.sleep(r.toLong)
+  }
 }
 
 object StopwatchTest {
