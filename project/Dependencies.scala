904564dd9053cbee6b5878e9ad3430b30d9ba8e4
Ryan Latture <rlatture@citrine.io>
2021-07-30 12:58:25 -0700

    Replaced netlib with breeze-natives. Upgraded breeze to v1.2.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,13 +1,12 @@
 import sbt._
 
 object Dependencies {
-  lazy val netlibVersion = "1.1.2"
   lazy val junitVersion = "4.13.1"
-  lazy val breezeVersion = "1.1"
+  lazy val breezeVersion = "1.2"
 
   val thetaDeps = Seq(
-    "com.github.fommil.netlib" % "all" % netlibVersion,
     "org.scalanlp" %% "breeze" % breezeVersion,
+    "org.scalanlp" %% "breeze-natives" % breezeVersion,
     "junit" % "junit" % junitVersion % "test",
     "com.novocode" % "junit-interface" % "0.11" % "test"
   )
