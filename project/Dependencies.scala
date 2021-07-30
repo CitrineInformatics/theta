48c14f0272edc9fc3b8c0c166b98bbd9c0fe3344
Ryan Latture <rlatture@citrine.io>
2021-07-30 13:30:13 -0700

    Add netlib as pom only dependency.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,12 +1,13 @@
 import sbt._
 
 object Dependencies {
+  lazy val netlibVersion = "1.1.2"
   lazy val junitVersion = "4.13.1"
   lazy val breezeVersion = "1.2"
 
   val thetaDeps = Seq(
+    "com.github.fommil.netlib" % "all" % netlibVersion pomOnly(),
     "org.scalanlp" %% "breeze" % breezeVersion,
-    "org.scalanlp" %% "breeze-natives" % breezeVersion,
     "junit" % "junit" % junitVersion % "test",
     "com.novocode" % "junit-interface" % "0.11" % "test"
   )
